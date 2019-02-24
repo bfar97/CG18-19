d8bcde2813f81517e511ac507c0e23a2140debaf
heyitslamer <cesar.hms.95@gmail.com>
2019-02-24 14:33:05 +0000

    Codigo da *esfera* e do *cone* - standardizado para que os pontos seguem todos as mesmas orientacoes - constantes e funcoes auxiliares definidos a partir de defines

---
+++ codigodoCesar/Esfera/cmake-build-debug/CMakeFiles/esfera.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/esfera.dir/main.cpp.o"
+  "esfera.pdb"
+  "esfera"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/esfera.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
