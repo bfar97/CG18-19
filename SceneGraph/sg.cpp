e989c1cdfe521fb6250f0886cc7f8e16f7dbc541
heyitslamer <cesar.hms.95@gmail.com>
2019-03-15 00:01:22 +0000

    Cenas do alem

--- SceneGraph/sg.cpp
+++ SceneGraph/sg.cpp
@@ -10,6 +10,15 @@
 
 using namespace std;
 
+
+struct ponto {
+	float a;
+	float b;
+	float c;
+};
+
+typedef struct ponto Ponto;
+
 /*
  * Class principal para codificacao de um SceneGraph basico
  * para os arrays que codificam certas rotacoes, etc a posicao 0 simboliza a coordenada x,
@@ -22,28 +31,25 @@ class SceneGraph {
 	array<float, 3> scale;
 	array<float, 3> trans;
 	array<float, 4> rot;
-	vector<string> modelos;
+	vector<vector<Ponto>> modelos;
 	vector<SceneGraph> filhos;
-	// variavel do pai e funcoes que as usam
-	// para ja ficam apesar de achar que nao sao precisas
-	SceneGraph *pai;
 	
 	public:
 		// apresentacao de metodos
 		SceneGraph();
-		void setScale( array<float, 3> );
-		void setTrans( array<float, 3> );
-		void setRot( array<float, 4> );
 		void draw();
 		
 		// metodos de implementacao direta
+		void setScale( array<float, 3> escala) { this->scale = escala; }
+		void setTrans( array<float, 3> transl) { this->trans = transl; }
+		void setRot( array<float, 4> rota) { this->rot = rota; }
 		array<float, 3> getScale() { return this->scale; } 
 		array<float, 3> getTrans() { return this->trans; } 
 		array<float, 4> getRot() { return this->rot; }
-	        SceneGraph getParent() { return *(this->pai); }
-		void setParent( SceneGraph c ) { this->pai = &c; }
+		vector<vector<Ponto>> getModelos() { return this-> modelos; }
+		vector<SceneGraph> getFilhos() { return this->filhos; }
 		void addFilho( SceneGraph c ) { this->filhos.push_back( c ); }
-		void addModelo( string c ) { this->modelos.push_back( c ); }
+		void addModelo( vector<Ponto> c ) { this->modelos.push_back( c ); }
 }; 
 
 SceneGraph::SceneGraph() {
@@ -51,28 +57,6 @@ SceneGraph::SceneGraph() {
 	scale.fill(1.0f);
 	trans.fill(0.0f);
 	rot.fill(0.0f);
-	pai = nullptr;
-
-}
-
-void SceneGraph::setScale(array<float, 3> escala) {
-
-	array<float, 3> aux = escala;
-	this->scale = aux;
-
-}
-
-void SceneGraph::setTrans(array<float, 3> escala) {
-
-	array<float, 3> aux = escala;
-	this->trans = aux;
-
-}
-
-void SceneGraph::setRot(array<float, 4> escala) {
-
-	array<float, 4> aux = escala;
-	this->rot = aux;
 
 }
 
@@ -83,27 +67,38 @@ void SceneGraph::draw() {
 	glRotatef(rot[3], rot[0], rot[1], rot[2]);
 	glTranslatef(trans[0], trans[1], trans[2]);
 	glScalef(scale[0], scale[1], scale[2]);
+	// Instrucao para mostar os  valores
+	
 	for(SceneGraph tmp : this->filhos ) {
 		tmp.draw();
 	}
 	glPopMatrix();
 }
 
-/*
+
 int main() {
 
 	SceneGraph teste;
-	SceneGraph teste2;
-	array<float, 3> h2 = {1.0f, 2.0f, 3.0f};
+	SceneGraph teste2 = teste;
+	SceneGraph teste3;
 	
-	teste2.setParent(teste);
+	array<float, 3> h2 = {1.0f, 2.0f, 3.0f};
+	array<float, 3> h3 = {10.0f, 10.0f, 10.0f};
+
 	cout << teste.getScale()[0] << '\n';
 	teste.getScale()[0] = 3;
 	cout << teste.getScale()[0] << '\n';
 	teste.setScale(h2);
+	teste.addFilho(teste3);
+	teste3.setScale(h3);
+
+	for( auto k : teste.getFilhos() ) {
+		cout << k.getScale()[1] << ' ' << teste3.getScale()[1] << '\n';
+	}
+
 	h2[1] = 5.0f;
 	cout << teste.getScale()[1] << '\n';
-	cout << teste2.getParent().getScale()[1] << '\n';
+	cout << teste2.getScale()[1] << '\n';
 	return 0;
 }
-*/
+
