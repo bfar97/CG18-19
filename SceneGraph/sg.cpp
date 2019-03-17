8c0c05a1b133663589040117c53345c120acccd3
heyitslamer <cesar.hms.95@gmail.com>
2019-03-17 22:41:01 +0000

    Scene Graph concluida

--- SceneGraph/sg.cpp
+++ SceneGraph/sg.cpp
@@ -1,104 +1,115 @@
-#include <vector>
-#include <string>
-#include <array>
-#include <iostream>
-#include <GL/glew.h>
-#include <GL/glut.h>
-#include <stdio.h>
-#include <stdlib.h>
-
+#include "sg.h"
 
 using namespace std;
 
-
-struct ponto {
-	float a;
-	float b;
-	float c;
-};
-
-typedef struct ponto Ponto;
-
-/*
- * Class principal para codificacao de um SceneGraph basico
- * para os arrays que codificam certas rotacoes, etc a posicao 0 simboliza a coordenada x,
- * a posicao 1 codifca a coordenada y, etc. No caso das rotacoes a posicao 3 codifica o angulo.
- *
- */
-class SceneGraph {
-	
-	// variaveis
-	array<float, 3> scale;
-	array<float, 3> trans;
-	array<float, 4> rot;
-	vector<vector<Ponto>> modelos;
-	vector<SceneGraph> filhos;
-	
-	public:
-		// apresentacao de metodos
-		SceneGraph();
-		void draw();
-		
-		// metodos de implementacao direta
-		void setScale( array<float, 3> escala) { this->scale = escala; }
-		void setTrans( array<float, 3> transl) { this->trans = transl; }
-		void setRot( array<float, 4> rota) { this->rot = rota; }
-		array<float, 3> getScale() { return this->scale; } 
-		array<float, 3> getTrans() { return this->trans; } 
-		array<float, 4> getRot() { return this->rot; }
-		vector<vector<Ponto>> getModelos() { return this-> modelos; }
-		vector<SceneGraph> getFilhos() { return this->filhos; }
-		void addFilho( SceneGraph c ) { this->filhos.push_back( c ); }
-		void addModelo( vector<Ponto> c ) { this->modelos.push_back( c ); }
-}; 
+// SceneGraph
+// Construtor
 
 SceneGraph::SceneGraph() {
 
-	scale.fill(1.0f);
-	trans.fill(0.0f);
-	rot.fill(0.0f);
+	this->scale.fill(1.0f);
+	this->trans.fill(0.0f);
+	this->rot.fill(0.0f);
 
 }
 
-void SceneGraph::draw() {
+// Setters 
+
+void SceneGraph::setScale( array<float, 3> escala) { 
+
+	this->scale = escala; 
+
+}
+
+void SceneGraph::setTrans( array<float, 3> transl ) { 
+
+	this->trans = transl; 
+
+}
+                
+void SceneGraph::setRot( array<float, 4> rota) { 
+
+	this->rot = rota; 
+
+}
+
+void SceneGraph::setModelo(vector<vector<Pontos>> pontos) {
+	
+	this->modelos = pontos;
+
+}
+
+// Getters
+                
+array<float, 3> SceneGraph::getScale() { 
+
+	return this->scale; 
+
+}
+                
+array<float, 3> SceneGraph::getTrans() {
+       	
+	return this->trans; 
+
+}
+                
+array<float, 4> SceneGraph::getRot() { 
+	
+	return this->rot; 
+
+}
+               
+vector<vector<Pontos>> SceneGraph::getModelos() { 
+	
+	return this-> modelos; 
+
+}
+                
+vector<SceneGraph> SceneGraph::getFilhos() { 
+	
+	return this->filhos; 
+
+}
+
+// Funcoes Adicionais
+                
+void SceneGraph::addFilho( SceneGraph c ) { 
+	
+	this->filhos.push_back( c ); 
+
+}
+               
+void SceneGraph::addModelo( vector<Pontos> c ) { 
+	
+	this->modelos.push_back( c ); 
+
+}
+
+// Funcao responsavel por desnhar a estrutra
+
+void SceneGraph::draw() const {
 
 	glPushMatrix();
-	// ordem ainda nao esta definida
-	glRotatef(rot[3], rot[0], rot[1], rot[2]);
-	glTranslatef(trans[0], trans[1], trans[2]);
-	glScalef(scale[0], scale[1], scale[2]);
-	// Instrucao para mostar os  valores
 	
-	for(SceneGraph tmp : this->filhos ) {
+	// ordem ainda nao esta definida, ainda vai ser melhorado
+	glScalef(scale[0], scale[1], scale[2]);
+	glRotatef(rot[0], rot[1], rot[2], rot[3]);
+	glTranslatef(trans[0], trans[1], trans[2]);
+	
+	glBegin(GL_TRIANGLES);
+	
+	for( vector<Pontos> const &pnts : this->modelos ) {	
+		for( Pontos const &p : pnts ) {
+        		glVertex3f(p.a, p.b, p.c);
+    		}
+	}
+
+	glEnd();
+
+	for( SceneGraph const &tmp : this->filhos ) {
 		tmp.draw();
 	}
+
 	glPopMatrix();
+
 }
-
-
-int main() {
-
-	SceneGraph teste;
-	SceneGraph teste2 = teste;
-	SceneGraph teste3;
-	
-	array<float, 3> h2 = {1.0f, 2.0f, 3.0f};
-	array<float, 3> h3 = {10.0f, 10.0f, 10.0f};
-
-	cout << teste.getScale()[0] << '\n';
-	teste.getScale()[0] = 3;
-	cout << teste.getScale()[0] << '\n';
-	teste.setScale(h2);
-	teste.addFilho(teste3);
-	teste3.setScale(h3);
-
-	for( auto k : teste.getFilhos() ) {
-		cout << k.getScale()[1] << ' ' << teste3.getScale()[1] << '\n';
-	}
-
-	h2[1] = 5.0f;
-	cout << teste.getScale()[1] << '\n';
-	cout << teste2.getScale()[1] << '\n';
-	return 0;
-}
-
---
+++ SceneGraph/sg.h
@@ -0,0 +1,67 @@
+#ifndef __DEPENDS__
+#define __DEPENDS__
+#include <vector>
+#include <string>
+#include <array>
+#include <GL/glut.h>
+#endif
+
+#ifndef __ESTRUTURAS__
+#define __ESTRUTURAS__
+
+using namespace std;
+
+/*
+ * Estrutura usada para guardar as coordenadas de um ponto num espaço 3D 
+ */
+struct ponto {
+	float a;
+	float b;
+	float c;
+};
+
+typedef struct ponto Pontos;
+
+/*
+ * Class principal para codificacao de um SceneGraph basico
+ * para os arrays que codificam certas rotacoes, etc a posicao 0 simboliza a coordenada x,
+ * a posicao 1 codifca a coordenada y, etc. No caso das rotacoes a posicao 0 codifica o angulo e de seguida sao dados os eixos de rotacao.
+ *
+ * A funcao de desenho considera escalas primeiro, rotacoes de seguida e 
+ * finalmente translacoes
+ */
+class SceneGraph {
+
+        // variaveis
+        array<float, 3> scale;
+        array<float, 3> trans;
+        array<float, 4> rot;
+        vector<vector<Pontos>> modelos;
+        vector<SceneGraph> filhos;
+        
+        public:
+                // Construtores 
+                SceneGraph();
+
+		// Setters
+                void setScale( array<float, 3> ); 
+                void setTrans( array<float, 3> );
+                void setRot( array<float, 4> ); 
+                void setModelo( vector<vector<Pontos>> );
+
+		// Getters
+                array<float, 3> getScale(); 
+                array<float, 3> getTrans();
+                array<float, 4> getRot();
+                vector<vector<Pontos>> getModelos();
+                vector<SceneGraph> getFilhos();
+
+		// Funcoes adicionais
+                void addFilho( SceneGraph ); 
+                void addModelo( vector<Pontos> );
+
+		// Funcao responsavel por desenhar a estrutura
+                void draw() const;
+};
+
+#endif
